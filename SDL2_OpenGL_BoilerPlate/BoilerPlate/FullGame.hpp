39ff998221662540ca132c4204f1d6ed9071a21b
lmq <luisqm22@gmail.com>
2018-02-28 09:45:47 -0400

    full game

--- SDL2_OpenGL_BoilerPlate/BoilerPlate/FullGame.hpp
+++ SDL2_OpenGL_BoilerPlate/BoilerPlate/FullGame.hpp
@@ -1,4 +1,19 @@
 #pragma once
+
+#include <vector>
+#include "Vector2.hpp"
+#include "PlayerShip.h"
+#include "Asteroides.hpp"
+#include "MathUtilities.hpp"
+
+namespace Game
+{
+	namespace Herramientas
+	{
+
+	}
+}
+
 class FullGame
 {
 public:
